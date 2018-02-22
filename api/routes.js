d7fe1a0406c74adf481fcab390b54554f5fea2bd
Ciro Chang <cirochang@live.com>
2018-02-22 03:41:34 -0300

    fix deprecation of syntaxy of send

--- api/routes.js
+++ api/routes.js
@@ -4,18 +4,17 @@ module.exports = function(app) {
 
   app.route('/api/v1/forecast').get((req, resOrigin) => {
     request(`${process.env.WEATHER_API_URI}/forecast?q=${req.query.cityName}&APPID=${process.env.WEATHER_API_KEY}`, { json: true }, (err, res, body) => {
-      console.log(body);
       if (err)
-        return resOrigin.send(500, err);
-      return resOrigin.send(200, body);
+        return resOrigin.status(500).send(err);
+      return resOrigin.status(200).send(body);
     });
   });
 
   app.route('/api/v1/current_weather').get((req, resOrigin) => {
     request(`${process.env.WEATHER_API_URI}/weather?q=${req.query.cityName}&APPID=${process.env.WEATHER_API_KEY}`, { json: true }, (err, res, body) => {
       if (err)
-        return resOrigin.send(500, err);
-      return resOrigin.send(200, body);
+        return resOrigin.status(500).send(err);
+      return resOrigin.status(200).send(body);
     });
   });
 
